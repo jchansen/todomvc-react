f36790bf519909b730ef131ef54bbf794ad72711
jchansen <jason@Jasons-MacBook-Pro.local>
2014-02-08 00:20:37 -0700

    cleanup

--- public/app/app.js
+++ public/app/app.js
@@ -4,10 +4,6 @@ define(
 	],
 	function (Backbone) {
 
-		// if(typeof window !== "undefined" && window !== null){
-		// TODO: Implement in memory store for testing purposes
-		// https://code.google.com/p/awad/source/browse/trunk/www/air/backbone/memorystore.js?r=18
-
 		var app = {
       start: function(){
         Backbone.history.start({pushState: true});
